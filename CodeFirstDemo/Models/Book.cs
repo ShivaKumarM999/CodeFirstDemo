4bf34836e8bc37cbf2b85542e283bdd390cd14b4
ShivaKumarM999 <81612983+ShivaKumarM999@users.noreply.github.com>
2021-03-31 16:48:45 +0530

    Add project files.

---
+++ CodeFirstDemo/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstDemo.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public int Price { get; set; }
+
+        public string Publisher { get; set; }
+
+    }
+}
---
+++ CodeFirstDemo/Models/BooksDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstDemo.Models
+{
+    public class BooksDbContext: DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
